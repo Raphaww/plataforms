633535f1cb764f1e87b5e1534e016babb94f027e
Raphael Isidro <raphaww@gmail.com>
2023-02-02 13:37:28 -0500

    - raphaww

--- middleware.ts
+++ middleware.ts
@@ -18,14 +18,14 @@ export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers.get("host") || "demo.vercel.pub";
+  const hostname = req.headers.get("host") || "demo.raphaww.com";
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
 
   // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.vercel.pub");
+  if (hostname === "raphaww.com" || hostname === "platforms.vercel.app") {
+    return NextResponse.redirect("https://demo.raphaww.com");
   }
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
@@ -35,8 +35,8 @@ export default async function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "")
-          .replace(`.platformize.vercel.app`, "")
+          .replace(`.raphaww.com`, "")
+          // .replace(`.platformize.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
 
   // rewrites for app pages
@@ -55,7 +55,7 @@ export default async function middleware(req: NextRequest) {
   }
 
   // rewrite root application to `/home` folder
-  if (hostname === "localhost:3000" || hostname === "platformize.vercel.app") {
+  if (hostname === "localhost:3000"){ // || hostname === "platformize.vercel.app") {
     return NextResponse.rewrite(new URL(`/home${path}`, req.url));
   }
 
